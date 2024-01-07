6199b7bb6c8cc1c7f1d1d8269eb7395e992490f9
Sriram Suresh <100552311+Sriramsureshkumar@users.noreply.github.com>
2024-01-07 10:52:05 +0530

    Update header.jsx

--- developers/src/components/header.jsx
+++ developers/src/components/header.jsx
@@ -3,7 +3,7 @@ import "../styles/style.css";
 function Header() {
     return (
         <div className="head">
-            <h1>Generate Admin Credential- InVITe</h1>
+            <h1>Generate Admin Credential- EventFlow</h1>
             <center>
                 <p>
                     Currently in test mode so all the new admin assigned will 
