d08e86a4b59675e7f6ccc333208289404c510c6c
Sriram Suresh <100552311+Sriramsureshkumar@users.noreply.github.com>
2024-01-07 12:20:15 +0530

    Update [eventId].jsx

--- client/src/pages/event/[eventId].jsx
+++ client/src/pages/event/[eventId].jsx
@@ -187,16 +187,12 @@ function EventPage() {
                                     <h3 className="text-xl font-semibold text-gray-900 mb-2">
                                         About the Event
                                     </h3>
-                                    {Array(1)
-                                        .fill()
-                                        .map((_, index) => (
                                             <p
                                                 key={index}
                                                 className="text-gray-600 text-md"
                                             >
                                                 {eventData.description}
                                             </p>
-                                        ))}
                                 </div>
                                 {!isTechnicalOrEducationalEvent?(<div className="mb-4 bg-white px-6 py-4 rounded-lg shadow-md">
                                     <h3 className="text-xl font-semibold text-gray-900 mb-2">
