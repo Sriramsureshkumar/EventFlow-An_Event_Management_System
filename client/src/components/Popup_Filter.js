9d0a151473b72edf82bc9c20f74edc856accdc1b
Sriram Suresh <100552311+Sriramsureshkumar@users.noreply.github.com>
2024-02-03 00:07:05 +0530

    Update Popup_Filter.js

--- client/src/components/Popup_Filter.js
+++ client/src/components/Popup_Filter.js
@@ -81,9 +81,14 @@ function Popup_Filter({
                         className="filterInput"
                     >
                         <option value="">Select a category...</option>
-                        <option value="category1">Category 1</option>
-                        <option value="category2">Category 2</option>
-                        <option value="category3">Category 3</option>
+                        <option value="Technical">Technical</option>
+                        <option value="Comic">Comic</option>
+                        <option value="Personalized">Personalized</option>
+                        <option value="Entertainment">Entertainment</option>
+                        <option value="Educational">Educational</option>
+                        <option value="Workshop">Workshop</option>
+                        <option value="Music">Music</option>
+                        <option value="Others">Others</option>
                     </select>
                 </div>
                 {/* Input field to filter through a date range */}
