6be6db3eea7dcdca73a44bef65f4fd167b8225b2
Sriram Suresh <100552311+Sriramsureshkumar@users.noreply.github.com>
2024-04-25 18:15:56 +0530

    Update setAdminToken.js

--- client/src/utils/setAdminToken.js
+++ client/src/utils/setAdminToken.js
@@ -5,7 +5,6 @@ const cookies = new Cookies();
 export const setAdminToken = (admin_id) => {
     console.log("admin id state value ", admin_id);
     cookies.set("admin_token", admin_id, {
-        expires: new Date(Date.now() + 86400000),
-        // path: "/", commeting this becoz anurag daddy ne bola :)
+        expires: new Date(Date.now() + 86400000)
     });
 };
